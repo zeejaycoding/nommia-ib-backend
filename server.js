99f1138f0e3dea6159f02c7625f8887f68df14f7
Zahab Jahangir <zahabjahangir27@gmail.com>
2026-02-11 01:35:04 +0500

    deploy commit

--- server.js
+++ server.js
@@ -414,7 +414,4 @@ app.use((req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`[Server] Environment: ${process.env.NODE_ENV || 'development'}`);
-});
+export default app;
